267bbeec2fbc178ff38435e513383411f6bbdd9a
Asadur Zaman <32617400+AsadurS@users.noreply.github.com>
2021-03-14 17:34:17 +0600

    draft

---
+++ database/migrations/2021_03_14_111718_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('title');
+            $table->float('price');
+            $table->longText('description');
+            $table->string('image');
+            $table->foreignIdFor(User::class)->nullable()->constrained()->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
