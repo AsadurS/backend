b30a221afebd3e0918982d325ea29656aab24cce
Asadur <32617400+AsadurS@users.noreply.github.com>
2021-03-18 17:49:37 +0600

    code refactoring

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use App\Http\Resources\ProductResource;
 use App\Http\Requests\CreateProductRequest;
 use App\Http\Requests\UpdateProductRequest;
-use Illuminate\Support\Facades\Storage;
 use Exception;
 
 class ProductController extends Controller
 {
+
+    /**
+     * store product
+     *
+     * @param CreateProductRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(CreateProductRequest $request, $success = true)
     {
         try {
@@ -35,20 +40,25 @@ class ProductController extends Controller
         return response()->json(['success' => $success]);
     }
 
+    /**
+     * update product
+     *
+     * @param UpdateProductRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function update(UpdateProductRequest $request, $product)
     {
         try {
             $product =  Product::where('uuid', $product)->first();
             if ($request->file('image')) {
                 $fileName = time() . '_' . $request->image->getClientOriginalName();
-                // Storage::delete(public_path('public/image/product', $product->image));
                 $request->file('image')->storeAs('public/image/product', $fileName);
             }
             $success = $product->update([
                 'title' => $request->title,
                 'description' => $request->description,
                 'price' => $request->price,
-                "image" => $fileName,
+                "image" => $fileName ??  $product->image ,
             ]);
         } catch (Exception $excption) {
             return $excption;
@@ -58,6 +68,10 @@ class ProductController extends Controller
         return response()->json(['success' => $success]);
     }
 
+    /**
+     * manage product
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function manage($success = true)
     {
 
@@ -71,16 +85,20 @@ class ProductController extends Controller
         return  response()->json(['success' => $success, 'products' => $products]);
     }
 
+    /**
+     * edit  product
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function edit($product)
     {
 
         $product = Product::where('uuid', $product)->first();
-        return response()->json(['product' => new ProductResource($product)]);
+        return response()->json(['product' => $product]);
     }
-
-/**
- * 
- */
+    /**
+    * edit  delete product
+    * @return \Illuminate\Http\JsonResponse
+    */
     public function delete($product)
     {
         try {
--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -14,12 +14,12 @@ class UpdateProductRequest extends FormRequest
      * @return array
      */
     public function rules()
-    {
+    {   
         return [
             'price'       => 'required|numeric|gt:0',
             'description' => 'required|string|min:15',
             'title'       => 'required|string|min:3|max:255',
-            'image'       => 'nullable|mimes:jpeg,jpg,png,gif|max:10000'
+            // 'image'       => 'nullable|mimes:jpeg,jpg,png,gif|max:10000'
         ];
     }
 }
